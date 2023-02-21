4adf34209c87065b283fbc6f0e20d04e80c21329
MillicentKinoti <camillekinoti19@gmail.com>
2023-02-21 17:54:48 +0300

    main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,31 @@
+#ifndef MAIN_H
+
+#define MAIN_H
+
+
+/*0-putchar.c */
+int _putchar(char c);
+/*1-alphabet.c */
+void print_alphabet(void);
+/*2-print_alphabet+x10.c*/
+void print_alphabet_x10(void);
+/*3-islower.c */
+int _islower(int c);
+/*4-isalpha.c */
+int _isalpha(int c);
+/* 5-sign.c*/
+int print_sign(int n);
+/* 6-abs.c */
+int _abs(int);
+/*7-print_last_digit.c*/
+int print_last_digit(int);
+/*8-24_hours.c */
+void jack_bauer(void);
+/*9-void times_table(void);*/
+void times_table(void);
+/*10-add.c */
+int add(int, int);
+/* 11-print_to_98.c */
+void print_to_98(int);
+
+#endif
