b70d20977a2d2897b7de3d422c77ac48122c320f
MillicentKinoti <camillekinoti19@gmail.com>
2023-02-21 10:02:10 +0300

    first commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet in lowercase
+ **/
+void print_alphabet_x10(void)
+{
+int i = 0;
+char l;
+while (i++ <= 9)
+{
+for (l = 'a'; l <= 'z'; l++)
+_putchar(l);
+_putchar('\n');
+}
+}
