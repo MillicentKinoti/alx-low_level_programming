a3d176f932e0b4c655f3433ba9e02b8452011378
MillicentKinoti <camillekinoti19@gmail.com>
2023-02-22 03:51:42 +0300

    first commit

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+/*1-main.c */
+void positive_or_negative(int i);
+/*2-largest_number.c*/
+int largest_number(int a, int b, int c);
+/*3-print_remaininf_days.c*/
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+
+
+#endif 
