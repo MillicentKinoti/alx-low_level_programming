e40f970e418f06ffe18ab4fa67c9b7f184bf21bc
MillicentKinoti <camillekinoti19@gmail.com>
2023-02-23 10:29:24 +0300

    first commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - checks for upperchase character
+ * @c: alphabet
+ * Return: 1 if uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+int _putchar(char c);
+/*0-isupper.c*/
+int _isupper(int c);
+
+
+#endif
+
