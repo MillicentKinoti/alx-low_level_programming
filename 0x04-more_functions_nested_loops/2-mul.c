bf5a76d8a513e3d5b2d418b182899a35c6de6568
MillicentKinoti <camillekinoti19@gmail.com>
2023-02-23 10:39:41 +0300

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - returns product of a and b
+ * @a :  first integer
+ * @b : second integer
+ * Return:  product of a and b
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -10,6 +10,9 @@ int _isupper(int c);
 /*1-isdigit.c*/
 int _isdigit(int c);
 
+/*2-mul-c*/
+int mul(int a, int b);
+
 
 
 
