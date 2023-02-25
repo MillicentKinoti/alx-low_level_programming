e26e8c86fe3d50fd0e32c5b401e1ecc27e2ede84
MillicentKinoti <camillekinoti19@gmail.com>
2023-02-25 21:56:03 +0300

    main

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,7 +6,7 @@
 void positive_or_negative(int i);
 /*2-largest_number.c*/
 int largest_number(int a, int b, int c);
-/*3-print_remaininf_days.c*/
+/*3-print_remaining_days.c*/
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 
