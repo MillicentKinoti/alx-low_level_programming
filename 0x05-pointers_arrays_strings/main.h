cd4f43133c16269cd43e8b2ad8c2d0a8a0c27f2b
MillicentKinoti <camillekinoti19@gmail.com>
2023-02-27 22:39:05 +0300

    pointers

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -11,4 +11,14 @@ int _strlen(char *s);
 void _puts(char *str);
 /*4-print_rev.c*/
 void print_rev(char *s);
+/*5-rev_string.c*/
+void rev_string(char *s);
+/*6-puts2.c*/
+void puts2(char *str);
+/*7-puts_half.c*/
+void puts_half(char *str);
+/*8-print_array.c*/
+void print_array(int *a, int n);
+/*9-strcpy.c*/
+char *_strcpy(char *dest, char *src);
 #endif
